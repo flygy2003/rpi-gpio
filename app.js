8b48e40b2bdf5f49d5d727c48f4f6c3059065ea5
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-06-29 21:02:00 -0700

    No while loop on blinker; 32 still

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ var gpio = require('rpi-gpio');
 gpio.setup(32, gpio.DIR_OUT, write);
 
 function write() {
-    while (true) {
-      setTimeout(function() {
-        gpio.write(32, true, function(err) {
-          if (err) throw err;
-        });
-      }, 3000);
-      setTimeout(function() {
-        gpio.write(32, true, function(err) {
-          if (err) throw err;
-        });
-      }, 2000);
-    }
+    setTimeout(function() {
+      gpio.write(32, true, function(err) {
+        if (err) throw err;
+      });
+    }, 3000);
+    setTimeout(function() {
+      gpio.write(32, false, function(err) {
+        if (err) throw err;
+      });
+    }, 2000);
+    setTimeout(function() {
+      gpio.write(32, true, function(err) {
+        if (err) throw err;
+      });
+    }, 3000);
+    setTimeout(function() {
+      gpio.write(32, false, function(err) {
+        if (err) throw err;
+      });
+    }, 2000);
+    setTimeout(function() {
+      gpio.write(32, true, function(err) {
+        if (err) throw err;
+      });
+    }, 3000);
+    setTimeout(function() {
+      gpio.write(32, false, function(err) {
+        if (err) throw err;
+      });
+    }, 2000);
 }
