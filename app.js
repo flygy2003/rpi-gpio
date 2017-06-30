a013817e46c1d68d012bf8674bf7d97e6e757563
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-06-30 11:31:57 -0700

    Initalizing all switches with pin array

--- app.js
+++ app.js
@@ -1,30 +1,73 @@
-var gpio = require('rpi-gpio');
+var io = require('rpi-gpio');
 
-gpio.setup(32, gpio.DIR_OUT, () => {
-  var seconds = 0;
-  var isOn = false;
-  const interval = setInterval( () => {
-    seconds++
-    if(seconds == 3) {
-      if(isOn) {
-        gpio.write(32, false)
-      }else {
-        gpio.write(32, true)
-      }
-      isOn = !isOn
-      seconds = 0
-    }
-  }, 1000 )
-} )
+var pin = [3, 5, 7,
+            8, 10, 12,
+            11, 13, 15,
+            16, 18, 19,
+            22, 24, 26,
+            29, 31, 32,
+            33, 35, 37,
+            38, 40]
+io.setup(pin[0], DIR_OUT, () => {
 
-// function write() {
-//   gpio.write(32, true, off);
-//     setTimeout(function() {
-//     }, 3000);
-// }
-//
-// function off() {
-//   gpio.write(32, false, write);
-//   setTimeout(function() {
-//   }, 1000);
-// }
+})
+
+io.setup(pin[1], DIR_OUT, () => {
+
+})
+
+io.setup(pin[2], DIR_OUT, () => {
+
+})
+
+io.setup(pin[3], DIR_OUT, () => {
+
+})
+
+io.setup(pin[4], DIR_OUT, () => {
+
+})
+
+io.setup(pin[5], DIR_OUT, () => {
+
+})
+
+io.setup(pin[6], DIR_OUT, () => {
+
+})
+
+io.setup(pin[7], DIR_OUT, () => {
+
+})
+
+io.setup(pin[8], DIR_OUT, () => {
+
+})
+
+io.setup(pin[9], DIR_OUT, () => {
+
+})
+
+io.setup(pin[10], DIR_OUT, () => {
+
+})
+
+io.setup(pin[11], DIR_OUT, () => {
+
+})
+
+io.setup(pin[12], DIR_OUT, () => {
+
+})
+
+io.setup(pin[13], DIR_OUT, () => {
+
+})
+
+io.setup(pin[14], DIR_OUT, () => {
+
+})
+
+io.setup(pin[15], DIR_OUT, () => {
+
+})
