c848e9926e96eb5e663b29d7666e8a51c3f7e945
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-06-29 21:00:08 -0700

    Looped blinker on pin 32

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var gpio = require('rpi-gpio');
 gpio.setup(32, gpio.DIR_OUT, write);
 
 function write() {
-    gpio.write(32, true, function(err) {
-        if (err) throw err;
-    });
+    while (true) {
+      setTimeout(function() {
+        gpio.write(32, true, function(err) {
+          if (err) throw err;
+        });
+      }, 3000);
+      setTimeout(function() {
+        gpio.write(32, true, function(err) {
+          if (err) throw err;
+        });
+      }, 2000);
+    }
 }
