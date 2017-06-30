d2f588e97e9eefde04cca917a5ff920b4ec13636
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-06-30 11:36:26 -0700

    indented and pretty

--- app.js
+++ app.js
@@ -8,66 +8,67 @@ var pin = [3, 5, 7,
             29, 31, 32,
             33, 35, 37,
             38, 40]
-io.setup(pin[0], DIR_OUT, () => {
 
+io.setup(pin[0], DIR_OUT, () => {
+  
 })
 
 io.setup(pin[1], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[2], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[3], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[4], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[5], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[6], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[7], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[8], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[9], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[10], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[11], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[12], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[13], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[14], DIR_OUT, () => {
-
+  
 })
 
 io.setup(pin[15], DIR_OUT, () => {
-
+  
 })
