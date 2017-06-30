b7cd884ed7b81b8ff99141948296da881e392964
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-06-30 08:44:15 -0700

    nikin

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ var gpio = require('rpi-gpio');
 
 gpio.setup(32, gpio.DIR_OUT, write);
 
-function write() {
-    setTimeout(function() {
-      gpio.write(32, true, off);
-    }, 1000);
-}
+// function write() {
+//   gpio.write(32, true, off);
+//     setTimeout(function() {
+//     }, 3000);
+// }
+//
+// function off() {
+//   gpio.write(32, false, write);
+//   setTimeout(function() {
+//   }, 1000);
+// }
 
-function off() {
-  setTimeout(function() {
-    gpio.write(32, false, write);
-  }, 3000);
-}
+var seconds = 0;
+var isOn = false;
+const interval = setInterval( () => {
+  seconds++
+  if(seconds % 3 == 0) {
+    gpio.write(32, true)
+  }else if (seconds % 2 == 0) {
+    gpio.write(32, false)
+  }
+}, 1000 )
